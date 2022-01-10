fa0657b558d058b25f6699edc8e4199d02b32725
mykolao <mykolaoleskiv@gmail.com>
2022-01-10 13:19:18 +0200

    deleted everything

--- src/components/Hello.ts
+++
@@ -1,9 +0,0 @@
-import "../styles/greeting.css";
-const Hello = (msg: string) => {
-  const element = document.createElement("h1");
-  element.className = "greeting";
-  element.textContent = msg;
-  return element;
-};
-
-export default Hello;
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import "./styles/index.css";
-import "@csstools/normalize.css";
-import Hello from "./components/Hello";
-
-const root = document.querySelector(".root");
-root?.appendChild(Hello("Hello Typescript"));
