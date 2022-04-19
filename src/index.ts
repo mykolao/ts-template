d71dfd2fb6c1dbc4250ddd3482768b632fe48a74
mykolao <mykolaoleskiv@gmail.com>
2022-04-19 18:03:07 +0300

    chore: add prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const hello = document.createElement("h1");
-hello.textContent = "Hello Typescript ❤️";
-hello.style.color = "white";
-hello.style.fontSize = "4rem";
-document.querySelector(".container")?.appendChild(hello);
+const hello = document.createElement('h1');
+hello.textContent = 'Hello Typescript ❤️';
+hello.style.color = 'white';
+hello.style.fontSize = '4rem';
+document.querySelector('.container')?.appendChild(hello);
